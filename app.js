48e98d9bab81f621c5d1083d6716025033f06918
tr_h3x <travis.hetzel@gmail.com>
2022-05-31 16:34:58 -0500

    Complete the exercise
    an <h3> with blue text that says “I’m a blue h3!”
    
    a <div> with a black border and pink background color with the following elements inside of it:
    
    another <h1> that says “I’m in a div”
    
    a <p> that says “ME TOO!”
    
    Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.

--- app.js
+++ app.js
@@ -14,3 +14,34 @@ p.style.color = 'red';
 p.textContent = "Hey, I'm red!";
 
 container.appendChild(p);
+
+const blueH3 = document.createElement('h3');
+blueH3.style.color = 'blue';
+blueH3.textContent = "I'm a blue h3!"
+
+container.appendChild(blueH3);
+
+///new div step 3
+
+const div = document.createElement('div');
+div.setAttribute('style', 'color: blue; background: pink; border: black;');
+div.style.border = "thick solid black";
+//div.textContent = "attempt 1";
+
+//container.appendChild(div);
+
+const divH1 = document.createElement('h1');
+divH1.style.color = 'blue';
+divH1.textContent = "I'm in a div";
+
+div.appendChild(divH1);
+
+const p1 = document.createElement('p'); 
+p1.style.color = 'red';
+p1.textContent = "Me, too!";
+
+div.appendChild(p1);
+
+container.appendChild(div);
+
+//link for exercise https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events
